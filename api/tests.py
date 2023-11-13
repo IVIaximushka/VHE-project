bdaf2ce34c1cbc505691ef00b3d04fa78b8c358c
unknown <dar5114@mail.ru>
2023-11-13 22:16:02 +0300

    + тесты для профилей

--- api/tests.py
+++ api/tests.py
@@ -3,13 +3,14 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 
-from web.models import Genre, User
+from web.models import Genre, User, UserProfile
 
 
 class GenreTests(APITestCase):
     def setUp(self) -> None:
         self.user = User.objects.create(username='max', password='123',
-                                   email='t@t.ru', is_active=True)
+                                        email='t@t.ru', is_active=True)
+        self.user_profile = UserProfile.objects.create(user=self.user, is_author=True)
         self.genre = Genre.objects.create(title='genre1')
         self.token = Token.objects.create(user=self.user)
         self.token.save()
@@ -39,4 +40,19 @@ class GenreTests(APITestCase):
                                    data={'title': 'new_title'})
         self.assertEqual(Genre.objects.count(), 1)
         self.assertEqual(Genre.objects.get(id=self.genre.id).title, 'new_title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_profile_create(self):
+        response = self.client.post(reverse('create_profile'),
+                                    data={'username': 'test',
+                                          'email': '123@mail.ru',
+                                          'password': '123',
+                                          'is_author': 'True'})
+        self.assertEqual(UserProfile.objects.count(), 2)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_profile_list(self):
+        response = self.client.get(reverse('profile'),
+                                   headers={'Authorization': f'Token {self.token.key}'})
+        self.assertEqual(UserProfile.objects.count(), 1)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
